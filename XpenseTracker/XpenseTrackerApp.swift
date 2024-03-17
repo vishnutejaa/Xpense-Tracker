48cafd9d9c1679b59d785bb66bb355f30144f3cd
Vishnu <nandam.vishnutejaa@gmail.com>
2024-03-17 15:31:45 -0700

    Initial Commit

---
+++ XpenseTracker/XpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  XpenseTrackerApp.swift
+//  XpenseTracker
+//
+//  Created by Vishnu Tejaa on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct XpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
